4864bda786627b349e760eddc2aabc206ce04e44
Esther Corte Real <ecortereal45@gmail.com>
2025-05-12 15:48:27 -0300

    atualizaçoes na pagina principal - adcao de carrossel, mudança no footer // pagina de postagens ( tem q atualizar o footer)
     TUDO AINDA SEM O DJANGO - ATUALIZAR DPS

---
+++ static/js/postagenssemdjango.js
@@ -0,0 +1,14 @@
+function expandirTexto(button) {
+    var texto = button.previousElementSibling;
+    texto.classList.toggle("expandidotexto"); 
+
+   
+    if (texto.classList.contains("expandidotexto")) {
+        button.textContent = "Ver menos";
+    } else {
+        button.textContent = "Ver mais";
+    }
+}
+
+
+  
