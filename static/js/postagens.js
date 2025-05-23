436b02a121e26382f09a8a9bda4affea566de146
Gustavo Coroa <gustavocoroademattos@gmail.com>
2025-05-23 08:21:48 -0300

    ajustes front

--- static/js/postagens.js
+++ static/js/postagens.js
@@ -10,5 +10,28 @@ function expandirTexto(button) {
     }
 }
 
+const slidesContainer = document.querySelector('.slides');
+const slides = Array.from(document.querySelectorAll('.slide'));
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+let currentIndex = 0;
+
+function updateCarousel() {
+  const offset = -currentIndex * 100;
+  slidesContainer.style.transform = `translateX(${offset}%)`;
+}
+
+prevBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  updateCarousel();
+});
+
+nextBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % slides.length;
+  updateCarousel();
+});
+
+// inicia no slide 0
+updateCarousel();
 
   
