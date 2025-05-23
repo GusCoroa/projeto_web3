33d51935b0c5e3e440434e4d1505266610c840af
Gustavo Coroa <gustavocoroademattos@gmail.com>
2025-05-23 08:22:30 -0300

    ajustes postagens

--- static/js/postagens.js
+++ static/js/postagens.js
@@ -9,29 +9,3 @@ function expandirTexto(button) {
         button.textContent = "Ver mais";
     }
 }
-
-const slidesContainer = document.querySelector('.slides');
-const slides = Array.from(document.querySelectorAll('.slide'));
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
-let currentIndex = 0;
-
-function updateCarousel() {
-  const offset = -currentIndex * 100;
-  slidesContainer.style.transform = `translateX(${offset}%)`;
-}
-
-prevBtn.addEventListener('click', () => {
-  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-  updateCarousel();
-});
-
-nextBtn.addEventListener('click', () => {
-  currentIndex = (currentIndex + 1) % slides.length;
-  updateCarousel();
-});
-
-// inicia no slide 0
-updateCarousel();
-
-  
